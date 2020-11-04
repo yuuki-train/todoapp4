6c3d603846808aa582f48f9e22bdf00b309cf304
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2020-11-04 14:20:02 +0900

    todoapp4-created

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
